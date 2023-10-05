3d7fd390fe53d2094230c8716a1d9c58668d5bf4
Altech Inferno <altechinferno@gmail.com>
2023-10-05 14:26:48 -0700

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""distributes an archive to your web servers, using the function do_deploy: 
-"""
+"""Distributes an archive to web servers using Fabric."""
 
 from fabric.api import *
-from datetime import datetime
 from os.path import exists
+from datetime import datetime
 
-env.hosts = ['34.229.55.2', '100.26.178.240']
+env.hosts = ['web-01.example.com', 'web-02.example.com']
+env.user = 'your_username'
+env.key_filename = '/path/to/your/ssh/key.pem'
 
 def do_deploy(archive_path):
     """Distribute an archive to web servers."""
@@ -14,20 +15,20 @@ def do_deploy(archive_path):
         return False
 
     try:
+        # Upload the archive to the /tmp/ directory of the web servers
+        put(archive_path, '/tmp/')
+
+        # Construct paths and filenames
         archive_filename = archive_path.split('/')[-1]
         archive_name_no_extension = archive_filename.split('.')[0]
         release_dir = f'/data/web_static/releases/{archive_name_no_extension}'
-        tmp_archive_path = f'/tmp/{archive_filename}'
 
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, tmp_archive_path)
-
-        # Create release directory and extract archive
+        # Create the release directory and extract the archive
         run(f'mkdir -p {release_dir}')
-        run(f'tar -xzf {tmp_archive_path} -C {release_dir}')
-        run(f'rm {tmp_archive_path}')
+        run(f'tar -xzf /tmp/{archive_filename} -C {release_dir}')
+        run(f'rm /tmp/{archive_filename}')
 
-        # Move files to appropriate location
+        # Move files to the appropriate location
         run(f'mv {release_dir}/web_static/* {release_dir}/')
         run(f'rm -rf {release_dir}/web_static')
 
