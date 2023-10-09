a48b55aed6bb59f7fc6302480c9d873f38260f16
Altech Inferno <altechinferno@gmail.com>
2023-10-08 20:18:07 -0700

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,48 @@
 #!/usr/bin/python3
-"""the function do_deploy: """
+""" script to distribute an archive to web servers """
+
 from fabric.api import *
-from datetime import datetime
 from os.path import exists
 
-# my ips
 env.hosts = ['100.26.170.176', '100.24.72.207']
 
-
 def do_deploy(archive_path):
-    """archive to my web servers
-    """
-
-    if exists(archive_path) is False:
+    """ Distribute an archive to web servers """
+    if not exists(archive_path):
         return False
 
-    fn = archive_path.split('/')[-1]
-    
-    no_tgz_dir = '/data/web_static/releases/' + "{}".format(fn.split('.')[0])
-    tmp_dir = "/tmp/" + fn
-
     try:
+        # Upload the archive to /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_tgz_dir))
-        run("tar -xzf {} -C {}/".format(tmp_dir, no_tgz_dir))
-        run("rm {}".format(tmp_dir))
-        run("mv {}/web_static/* {}/".format(no_tgz_dir, no_tgz_dir))
-        run("rm -rf {}/web_static".format(no_tgz_dir))
+
+        filename = archive_path.split('/')[-1]
+        no_extension = filename.split('.')[0]
+
+        remote_dir = "/data/web_static/releases/"
+        full_path = remote_dir + no_extension
+
+        # Create necessary directories
+        run("mkdir -p {}".format(full_path))
+
+        # Uncompress the archive to the target directory
+        run("tar -xzf /tmp/{} -C {}/".format(filename, full_path))
+
+        # Delete the uploaded archive
+        run("rm /tmp/{}".format(filename))
+
+        # Move the contents to the desired location
+        run("mv {}/web_static/* {}/".format(full_path, full_path))
+
+        # Remove the web_static directory
+        run("rm -rf {}/web_static".format(full_path))
+
+        # Delete the existing /data/web_static/current symbolic link
         run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_tgz_dir))
-        
+
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(full_path))
+
         return True
     except:
         return False
+
