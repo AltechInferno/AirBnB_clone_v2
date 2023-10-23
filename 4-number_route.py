6553be0f0a33656e95ce46174d92d8152a22aaf0
Altech Inferno <altechinferno@gmail.com>
2023-10-23 01:24:08 +0000

    task4

---
+++ 4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""launches a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """prints Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints Hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """prints C and value of the text variable
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_magic(text='is cool'):
+    """prints python and value of the text
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """prints n is a number if n is an int
+    """
+    return '{:d} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
